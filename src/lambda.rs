a757ead034019afc9277b5d30c10a321ed843e94
James Dong <dong.james.sept14@gmail.com>
2017-12-31 13:37:28 -0600

    Lambda calculus module

---
+++ src/lambda.rs
@@ -0,0 +1,176 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum LambdaExpr {
+    Variable(String),
+    Apply(Box<LambdaExpr>, Box<LambdaExpr>),
+    Lambda(String, Box<LambdaExpr>),
+}
+
+impl LambdaExpr {
+    fn contains(&self, var: &str) -> bool {
+        match self {
+            &LambdaExpr::Variable(ref v) => v == var,
+            &LambdaExpr::Apply(ref e1, ref e2) => e1.contains(var) || e2.contains(var),
+            &LambdaExpr::Lambda(ref v, ref e) => v != var && e.contains(var),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Lexer<'a> {
+    contents: &'a str,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum TokenType {
+    Ident,
+    LParen,
+    RParen,
+    Lambda,
+    EOF,
+}
+
+impl TokenType {
+    fn can_begin_expr(&self) -> bool {
+        match *self {
+            TokenType::Ident => true,
+            TokenType::LParen => true,
+            TokenType::RParen => false,
+            TokenType::Lambda => true,
+            TokenType::EOF => false,
+        }
+    }
+}
+
+impl<'a> Lexer<'a> {
+    fn new(contents: &str) -> Lexer {
+        Lexer { contents: contents.trim_left() }
+    }
+
+    fn peek(&self) -> TokenType {
+        let ch = self.contents.chars().next();
+        if let Some(ch) = ch {
+            match ch {
+                '(' => TokenType::LParen,
+                ')' => TokenType::RParen,
+                '\\' => TokenType::Lambda,
+                'λ' => TokenType::Lambda,
+                _ => TokenType::Ident,
+            }
+        } else {
+            TokenType::EOF
+        }
+    }
+
+    fn consume(&mut self) -> Option<&str> {
+        match self.peek() {
+            TokenType::EOF => None,
+            TokenType::LParen | TokenType::RParen | TokenType::Lambda => {
+                // find index of next code point
+                let index = self.contents.char_indices().nth(1).map(|x| x.0).unwrap_or(self.contents.len());
+                self.contents = &self.contents[index..].trim_left();
+                None
+            },
+            TokenType::Ident => {
+                // find index of first non-word character
+                let index = self.contents.char_indices().find(|p| {
+                    let c = p.1;
+                    c == '(' || c == ')' || c == '\\' || c == 'λ' || c.is_whitespace()
+                }).map(|x| x.0).unwrap_or(self.contents.len());
+                let (ret, rest) = self.contents.split_at(index);
+                self.contents = rest.trim_left();
+                Some(ret)
+            },
+        }
+    }
+}
+
+fn parse_atom(lex: &mut Lexer) -> Result<LambdaExpr, String> {
+    match lex.peek() {
+        TokenType::Ident => Ok(LambdaExpr::Variable(lex.consume().unwrap().to_string())),
+        TokenType::LParen => {
+            lex.consume();
+            let result = parse_expr(lex)?;
+            if lex.peek() != TokenType::RParen {
+                Err(format!("expected RParen, got {:?}", lex.peek()))
+            } else {
+                lex.consume();
+                Ok(result)
+            }
+        },
+        TokenType::Lambda => {
+            lex.consume();
+            if lex.peek() != TokenType::Ident {
+                return Err(format!("expected Ident after Lambda, got {:?}", lex.peek()));
+            }
+            let param = lex.consume().unwrap().to_string();
+            Ok(LambdaExpr::Lambda(param, Box::new(parse_expr(lex)?)))
+        },
+        _ => Err(format!("expected expression, got {:?}", lex.peek()))
+    }
+}
+
+fn parse_expr(lex: &mut Lexer) -> Result<LambdaExpr, String> {
+    let mut tree = parse_atom(lex)?;
+    while lex.peek().can_begin_expr() {
+        tree = LambdaExpr::Apply(Box::new(tree), Box::new(parse_atom(lex)?));
+    }
+    Ok(tree)
+}
+
+pub fn parse(s: &str) -> Result<LambdaExpr, String> {
+    let mut lex: Lexer = Lexer::new(s);
+    parse_expr(&mut lex)
+}
+
+#[cfg(test)]
+mod tests {
+    use lambda::*;
+    use lambda::LambdaExpr::*;
+
+    fn x() -> String {
+        "x".to_string()
+    }
+
+    fn y() -> String {
+        "y".to_string()
+    }
+
+    #[test]
+    fn single_variable() {
+        assert_eq!(Ok(Variable(x())), parse("x"));
+    }
+
+    #[test]
+    fn single_lambda() {
+        assert_eq!(Ok(Lambda(x(), Box::new(Variable(x())))), parse("\\x x"));
+    }
+
+    #[test]
+    fn single_parenthesis() {
+        assert_eq!(Ok(Variable(x())), parse("(x)"));
+    }
+
+    #[test]
+    fn single_application() {
+        assert_eq!(Ok(Apply(Box::new(Variable(x())), Box::new(Variable(y())))), parse("x y"));
+    }
+
+    #[test]
+    fn church_add() {
+        assert_eq!(Ok(
+                Lambda("m".to_string(), Box::new(
+                        Lambda("n".to_string(), Box::new(
+                                Lambda("f".to_string(), Box::new(
+                                    Lambda("x".to_string(), Box::new(
+                                            Apply(
+                                                Box::new(Apply(
+                                                        Box::new(Variable("m".to_string())),
+                                                        Box::new(Variable("f".to_string())))),
+                                                Box::new(Apply(
+                                                        Box::new(Apply(
+                                                                Box::new(Variable("n".to_string())),
+                                                                Box::new(Variable("f".to_string())))),
+                                                        Box::new(Variable("x".to_string())))))))))))))),
+                parse("\\m \\n \\f \\x m f (n f x)"));
+    }
+}
