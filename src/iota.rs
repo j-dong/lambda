cf03f1a0c9bc34f9c9aa1fcfd2914ea222e2c21f
James Dong <dong.james.sept14@gmail.com>
2017-12-31 19:00:47 -0600

    Command-line interface

--- src/iota.rs
+++ src/iota.rs
@@ -3,3 +3,22 @@ pub enum IotaExpr {
     Apply(Box<IotaExpr>, Box<IotaExpr>),
     Iota,
 }
+
+impl IotaExpr {
+    fn append_to_string(&self, s: &mut String) {
+        match self {
+            &IotaExpr::Iota => { s.push('i'); },
+            &IotaExpr::Apply(ref e1, ref e2) => {
+                s.push('*');
+                e1.append_to_string(s);
+                e2.append_to_string(s);
+            },
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut ret = String::new();
+        self.append_to_string(&mut ret);
+        ret
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,43 @@ mod ski;
 mod iota;
 mod conv;
 
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("{}", ski::SKIExpr::from_lambda(lambda::parse("\\f f (\\x \\y \\z x z (y z)) (\\x \\y x)").unwrap()).unwrap());
+    println!("lambda calculus to iota converter");
+    println!("type 'quit' to quit");
+    let mut line = String::new();
+    loop {
+        print!("lc> ");
+        if let Err(err) = io::stdout().flush() {
+            eprintln!("error flushing stdout: {}", err);
+            return;
+        }
+        line.clear();
+        if let Err(err) = io::stdin().read_line(&mut line) {
+            eprintln!("error reading from stdin: {}", err);
+            return;
+        }
+        let line = line.trim();
+        if line == "quit" {
+            return;
+        }
+        let expr = match lambda::parse(line) {
+            Ok(x) => x,
+            Err(err) => {
+                eprintln!("syntax error: {}", err);
+                continue
+            },
+        };
+        print!("{} => ", expr);
+        let iota = match ski::SKIExpr::from_lambda(expr).map(iota::IotaExpr::from) {
+            Ok(x) => x,
+            Err(err) => {
+                eprintln!("error converting to SKI: {}", err);
+                continue
+            }
+        };
+        println!("{}", iota.to_string());
+    }
 }
